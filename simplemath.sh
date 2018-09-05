12111bcfee99199f7344b49280baa6175151136f
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-09-05 09:19:59 +0530

    add bash expressions

---
+++ simplemath.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# File: math.sh
+
+expr 5 + 2
+expr 5 - 2
+expr 5 \* 2
+expr 5 / 2
+
+
+echo "22 / 7" | bc -l
+echo "4.2 * 9.15" | bc -l
+echo "(6.5 / 0.5) +  (6 + 2.2)" | bc -l

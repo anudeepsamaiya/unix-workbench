e43bf303848de9b1a308d6b6a01a4b590d4e364f
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-09-10 18:47:46 +0530

    add conditional expressions

--- conditional-expressions.sh
+++ conditional-expressions.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+# File: repseq.sh
 
 echo "Start program"
 
@@ -9,3 +11,36 @@ else
 fi
 
 echo "End program"
+
+# odd=$(echo "$1 % 2" | bc)
+#
+# if [[ $odd -eq 0 ]]
+# then
+#   status="even"
+# else
+#   status="odd"
+# fi
+#
+# if [[ $1 -gt 0 ]] && [[ $1 -lt 10 ]]
+# then
+#   location="in"
+# else
+#   location="out of"
+# fi
+#
+# echo "This number is $status and $location range."
+
+# sequence=$(eval echo {$1..$2})
+# echo $sequence
+#
+# for i in $sequence
+# do
+#   compute=$(echo "$i % $3" | bc)
+#   echo $compute
+#   result="$result $compute"
+# done
+#
+# echo "result"
+# echo $result
+
+
